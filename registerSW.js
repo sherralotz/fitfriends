85a89560cd23d5732eee31900df036915d556c79
sherrajaye <sherrajayetuble@gmail.com>
2025-02-12 00:02:48 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/fitfriends/sw.js', { scope: '/fitfriends/' })})}
